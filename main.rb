6709488987191bfa7eeeb183b4a0fe8e0fc27524
Hiro Mataba <aganzemataba@gmail.com>
2021-10-08 16:01:42 +0200

    Create class App

--- main.rb
+++ main.rb
@@ -4,3 +4,11 @@ require_relative 'student'
 require_relative 'book'
 require_relative 'rental'
 require_relative 'classroom'
+
+class App
+  def initialize
+    @classroom = Classroom.new('Full-stack Devs')
+    @people = []
+    @books = []
+    @rentals = []
+  end
