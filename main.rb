0c4091d901646d98a1c00ec663e5dc624d7d44ee
Hiro Mataba <aganzemataba@gmail.com>
2021-10-08 16:04:26 +0200

    Add print_options method

--- main.rb
+++ main.rb
@@ -49,3 +49,15 @@ class App
       puts 'That is not a valid option'
     end
   end
+
+  def print_options
+    puts
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
