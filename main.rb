7bd6f013981e6d8853bdc15901f2bcbc4965cab8
Hiro Mataba <aganzemataba@gmail.com>
2021-10-08 16:03:22 +0200

    Add handle options method

--- main.rb
+++ main.rb
@@ -28,3 +28,24 @@ class App
 
     puts 'Thank you for using this app!'
   end
+
+  private
+
+  def handle_option(option)
+    case option
+    when '1'
+      list_books
+    when '2'
+      list_people
+    when '3'
+      create_person
+    when '4'
+      create_book
+    when '5'
+      create_rental
+    when '6'
+      list_rentals_by_person_id
+    else
+      puts 'That is not a valid option'
+    end
+  end
