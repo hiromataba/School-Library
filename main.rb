104e007d55edb1d15cc1b4ab281a75f0984dbaff
Tolibjon <tolibjohn@gmail.com>
2021-10-12 13:00:14 +0500

    Use DRY method for print_options

--- main.rb
+++ main.rb
@@ -47,14 +47,14 @@ class App
 
   def print_options
     puts
-    puts 'Please choose an option by entering a number: 😊'
-    puts '1 - List all books 📖 '
-    puts '2 - List all people 👪'
-    puts '3 - Create a person 👨'
-    puts '4 - Create a book 📖 '
-    puts '5 - Create a rental 🏠'
-    puts '6 - List all rentals for a given person id 👨'
-    puts '7 - Exit 🚫'
+    puts ['Please choose an option by entering a number: 😊',
+      '1 - List all books 📖 ',
+      '2 - List all people 👪',
+      '3 - Create a person 👨',
+      '4 - Create a book 📖 ',
+      '5 - Create a rental 🏠',
+      '6 - List all rentals for a given person id 👨',
+      '7 - Exit 🚫']
   end
 end
 
