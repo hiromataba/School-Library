2f928faeccb89479d12486d2ba198350f54c852f
Tolibjon <tolibjohn@gmail.com>
2021-10-14 12:24:28 +0500

    Add tests for instance variable values of class Person

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,1 +1,27 @@
-puts 'Test for person'
+require_relative '../person'
+
+describe Person do
+  before :each do
+    @person1 = Person.new(age: 25, name: 'Albert')
+    @person2 = Person.new(age: 15, name: 'Albert', parent_permission: false)
+  end
+
+  describe '#new' do
+    it "takes parameters and returns a Person object" do
+      expect(@person1).to be_an_instance_of(Person)
+    end
+  end
+
+  describe '#name' do
+    it "returns correct name" do
+      expect(@person1.name).to eql "Albert"
+    end
+  end
+
+  describe '#id' do
+    it "returns id of person" do
+      expect(@person1.id).to be_kind_of Numeric
+    end
+  end
+
+end
