d43f7cbbc9fb9b921cbf825b3037728db863fe7c
Tolibjon <tolibjohn@gmail.com>
2021-10-14 08:39:44 +0500

    Add tests for object class and instance object values

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,1 +1,27 @@
-puts 'Test book'
+require_relative '../book'
+
+describe Book do
+  before :each do
+    @book = Book.new 'Martin Eden', 'Jack London'
+  end
+
+  describe '#new' do
+    it "takes two parameters and returns a Book object" do
+      expect(@book).to be_an_instance_of(Book)
+    end
+  end
+
+  describe '#author' do
+    it "returns correct author" do
+      expect(@book.author).to eql "Jack London"
+    end
+  end
+
+  describe '#title' do
+    it "returns correct title" do
+      expect(@book.title).to eql "Martin Eden"
+    end
+  end
+
+  
+end
