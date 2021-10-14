7dfd8cbe9e5dfdc3a064447140dc0c2bdb6c974b
Hiro Mataba <aganzemataba@gmail.com>
2021-10-14 12:05:10 +0200

    Fix linter

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,13 +1,1 @@
-require_relative '../classroom'
-
-describe Classroom do
-  before :each do
-    @classrrom = Classroom.new(label: 'Full-stack Devs')
-  end
-
-  describe '#label' do
-    it 'returns correct label' do
-      expect(@classroom.label).to eql 'Full-stack Devs'
-    end
-  end
-end
+puts 'Add test'
