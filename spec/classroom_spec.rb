02cdbe0dd273c0cf1f1d0738f6acffb6f5f53030
Hiro Mataba <aganzemataba@gmail.com>
2021-10-14 11:11:55 +0200

    Add test for new instance of classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,1 +1,13 @@
-puts 'Add test for students'
+require_relative '../classroom'
+
+describe Classroom do
+  before :each do
+    @classrrom = Classroom.new(label: 'Full-stack Devs')
+  end
+
+  describe '#new' do
+    it 'takes parameter and returns a classroom' do
+      expect(@classroom).to be_an_instance_of(Classroom)
+    end
+  end
+end
