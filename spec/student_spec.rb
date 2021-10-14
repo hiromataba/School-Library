f7e4d1f4b3f267e9285b8d972fc6b564b4071f2e
Tolibjon <tolibjohn@gmail.com>
2021-10-14 13:41:27 +0500

    Add tests for Student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,1 +1,23 @@
-puts 'Test for student'
+require_relative '../student'
+require_relative '../classroom'
+
+describe Student do
+  before :each do
+    javascript = Classroom.new('JavaScript')
+    @student = Student.new(age: 33, classroom: javascript, name: 'Julia')
+   
+  end
+
+  describe '#name' do
+    it "returns correct name" do
+      expect(@student.name).to eql "Julia"
+    end
+  end
+
+  describe '#id' do
+    it "returns id of student" do
+      expect(@student.id).to be_kind_of Numeric
+    end
+  end
+
+end
